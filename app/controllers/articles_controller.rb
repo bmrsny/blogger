27d877e3f58bd6324cf1062b80ecf6a366543daa
Brandon Mrsny <bmrsny@gmail.com>
2014-12-24 11:36:45 -0700

    added the edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,18 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:id]).destroy
+    
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
   end
 end
