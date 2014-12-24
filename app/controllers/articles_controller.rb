4247bd20729bf1a3b3924de8cb8498ad4af3274e
Brandon Mrsny <bmrsny@gmail.com>
2014-12-24 11:47:23 -0700

    added some flash notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,12 +18,16 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params) #from our helper
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id]).destroy
-    
+
+    flash.notice = "Article '#{@article.title}' Destroyed!"
+
     redirect_to articles_path
   end
 
@@ -34,6 +38,8 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
 
     redirect_to article_path(@article)
   end
